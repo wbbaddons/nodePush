c4e344845db33a00e37fdd86a8a23b6484a60a43
Tim Düsterhus <timwolla@arcor.de>
2013-05-23 18:51:24 +0200

    Add support for deferred messages

--- file/lib/system/nodePush/NodePushHandler.class.php
+++ file/lib/system/nodePush/NodePushHandler.class.php
@@ -11,7 +11,14 @@ use wcf\util\StringUtil;
  * @package	be.bastelstu.wcf.nodePush
  * @subpackage	system.nodePush
  */
-class NodePushHandler extends \wcf\system\SingletonFactory {	
+class NodePushHandler extends \wcf\system\SingletonFactory {
+	/**
+	 * Array of messages to send at shutdown.
+	 * 
+	 * @var	array
+	 */
+	private $deferred = array();
+	
 	/**
 	 * Returns whether the push server is enabled (i.e. `NODEPUSH_HOST` is set).
 	 * 
@@ -52,7 +59,7 @@ class NodePushHandler extends \wcf\system\SingletonFactory {
 	/**
 	 * Connects to the inbound socket. Returns false if the connection failed.
 	 * 
-	 * @return boolean|resource
+	 * @return	boolean|resource
 	 */
 	private function connect() {
 		if (StringUtil::startsWith($this->getSocketPath(), 'unix://')) {
@@ -64,13 +71,13 @@ class NodePushHandler extends \wcf\system\SingletonFactory {
 	}
 	
 	/**
-	 * Sends a message to the connected clients. Returns true on success
-	 * and false otherwise.
+	 * Sends a message to the connected clients. Returns `true` on success and `false`
+	 * otherwise.
 	 * 
-	 * If $userIDs is an empty array the message will be set to every connected
-	 * client. Otherwise the message will only be sent to clients with the given userID.
+	 * If `$userIDs` is an empty array the message will be set to every connected client. 
+	 * Otherwise the message will only be sent to clients with the given userID.
 	 * 
-	 * ATTENTION: Do NOT (!) send any security related information via sendMessage. 
+	 * ATTENTION: Do NOT (!) send any security related information via sendMessage.
 	 * The userID given can easily be forged, by a malicious client!
 	 * 
 	 * @param	string			$message
@@ -100,6 +107,42 @@ class NodePushHandler extends \wcf\system\SingletonFactory {
 		}
 		
 		return (boolean) $success;
+	}
+	
+	/**
+	 * Registers a deferred message. Returns `true` on any well-formed message and `false`
+	 * otherwise.
+	 * Deferred messages will be sent on shutdown. This can be useful if your handler depends
+	 * on data that may not be written to database yet or to achieve a better performance as the
+	 * page is delivered first.
+	 * 
+	 * ATTENTION: Use this method only if your messages are not critical as you cannot check
+	 * whether your message was delivered successfully.
+	 * ATTENTION: Do NOT (!) send any security related information via sendDeferredMessage.
+	 * The userID given can easily be forged, by a malicious client!
+	 * 
+	 * @see	\wcf\system\nodePush\NodePushHandler::sendDeferredMessage()
+	 */
+	public function sendDeferredMessage($message, $userIDs = array()) {
+		if (!$this->isEnabled()) return false;
+		if (!\wcf\data\package\Package::isValidPackageName($message)) return false;
+		$userIDs = array_unique(\wcf\util\ArrayUtil::toIntegerArray($userIDs));
+		
+		$this->deferred[] = array(
+			'message' => $message,
+			'userIDs' => $userIDs
+		);
+		
+		return true;
+	}
+	
+	/**
+	 * Sends out the deferred messages.
+	 */
+	public function __destruct() {
+		foreach ($this->deferred as $data) {
+			$this->sendMessage($data['message'], $data['userIDs'], false);
+		}
 	}
 	
 	/**
