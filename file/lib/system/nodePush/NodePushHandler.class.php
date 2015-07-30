3a3b757949301a7aa7b4bd1b37d37d7e2b1c936b
Tim Düsterhus <tim@bastelstu.be>
2015-07-30 18:31:24 +0200

    Return true in isRunning when nodePush is enabled

--- file/lib/system/nodePush/NodePushHandler.class.php
+++ file/lib/system/nodePush/NodePushHandler.class.php
@@ -34,32 +34,7 @@ class NodePushHandler extends \wcf\system\SingletonFactory {
 	public function isRunning() {
 		if (!$this->isEnabled()) return false;
 		
-		try {
-			$sock = $this->connect();
-			if ($sock === false) return false;
-			
-			fclose($sock);
-		}
-		catch (\Exception $e) {
-			// gotta catch 'em all
-			try {
-				if (is_resource($sock)) fclose($sock);
-			}
-			catch (\Exception $e) { }
-			
-			return false;
-		}
-		
 		return true;
-	}
-	
-	/**
-	 * Connects to the inbound socket. Returns false if the connection failed.
-	 * 
-	 * @return	boolean|resource
-	 */
-	private function connect() {
-		return stream_socket_client(NODEPUSH_SOCKET, $errno, $errstr, 1);
 	}
 	
 	/**
