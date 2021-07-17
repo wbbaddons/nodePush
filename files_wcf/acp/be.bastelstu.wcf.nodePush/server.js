b8e41c779f6bf37b4ed24170eb19c28691e28440
Tim Düsterhus <tim@bastelstu.be>
2021-07-17 20:49:36 +0200

    Bump date in server.js

--- files_wcf/acp/be.bastelstu.wcf.nodePush/server.js
+++ files_wcf/acp/be.bastelstu.wcf.nodePush/server.js
@@ -1,5 +1,5 @@
 /*
- *  Copyright (C) 2012 - 2016 Tim Düsterhus
+ *  Copyright (C) 2012 - 2021 Tim Düsterhus
  *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU Affero General Public License as published by
